163b3cacd698dc722f0ccbc31574138b10e684de
Stepan Litvinov <stepan.litvinov@firstlinesoftware.com>
2015-02-25 15:49:21 +0300

    gridfs configuration

--- src/main/java/net/org/selector/storer/config/DatabaseConfiguration.java
+++ src/main/java/net/org/selector/storer/config/DatabaseConfiguration.java
@@ -2,8 +2,6 @@ package net.org.selector.storer.config;
 
 
 import com.mongodb.Mongo;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.context.annotation.Bean;
@@ -11,9 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
-import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import javax.inject.Inject;
 
@@ -21,25 +18,13 @@ import javax.inject.Inject;
 @Profile("!cloud")
 @EnableMongoRepositories("net.org.selector.storer.repository")
 @Import(value = MongoAutoConfiguration.class)
-public class DatabaseConfiguration extends AbstractMongoConfiguration  {
-
-    private final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
+public class DatabaseConfiguration extends AbstractMongoConfiguration {
 
     @Inject
     private Mongo mongo;
 
     @Inject
     private MongoProperties mongoProperties;
-
-    @Bean
-    public ValidatingMongoEventListener validatingMongoEventListener() {
-        return new ValidatingMongoEventListener(validator());
-    }
-
-    @Bean
-    public LocalValidatorFactoryBean validator() {
-        return new LocalValidatorFactoryBean();
-    }
 
     @Override
     protected String getDatabaseName() {
@@ -49,5 +34,10 @@ public class DatabaseConfiguration extends AbstractMongoConfiguration  {
     @Override
     public Mongo mongo() throws Exception {
         return mongo;
+    }
+
+    @Bean
+    public GridFsTemplate gridFsTemplate() throws Exception {
+        return new GridFsTemplate(mongoDbFactory(), mongoTemplate().getConverter());
     }
 }
