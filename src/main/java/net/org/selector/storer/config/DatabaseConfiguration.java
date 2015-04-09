d9f14465f0f54d72bede5804eaabab830fcb4b0c
Stepan Litvinov <stepan.litvinov@firstlinesoftware.com>
2015-04-09 19:51:13 +0300

    storer-4 Реализовать безопасность и кастомизацию для нескольких схем. Промежуточный коммит

--- src/main/java/net/org/selector/storer/config/DatabaseConfiguration.java
+++ src/main/java/net/org/selector/storer/config/DatabaseConfiguration.java
@@ -1,43 +1,109 @@
 package net.org.selector.storer.config;
 
 
+import com.google.common.base.Splitter;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import com.mongodb.Mongo;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.mongo.MongoProperties;
+import org.springframework.boot.bind.RelaxedPropertyResolver;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-import javax.inject.Inject;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.Map;
+import java.util.Set;
 
 @Configuration
-@Profile("!cloud")
-@EnableMongoRepositories("net.org.selector.storer.repository")
-@Import(value = MongoAutoConfiguration.class)
-public class DatabaseConfiguration extends AbstractMongoConfiguration {
+public class DatabaseConfiguration implements EnvironmentAware {
+    private Set<String> siteNames = Sets.newHashSet();
+    private RelaxedPropertyResolver propertyResolver;
+    private Set<AbstractMongoConfiguration> configurations = Sets.newHashSet();
+    private final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
+    private ImmutableMap<String, GridFsTemplate> gridFsTemplateRegister;
+    private ImmutableMap<String, MongoTemplate> mongoTemplateRegister;
 
-    @Inject
-    private Mongo mongo;
 
-    @Inject
-    private MongoProperties mongoProperties;
-
-    @Override
-    protected String getDatabaseName() {
-        return mongoProperties.getDatabase();
-    }
-
-    @Override
-    public Mongo mongo() throws Exception {
-        return mongo;
+    @Bean
+    public ImmutableMap<String, GridFsTemplate> gridFsTemplateRegister() throws Exception {
+        return gridFsTemplateRegister;
     }
 
     @Bean
-    public GridFsTemplate gridFsTemplate() throws Exception {
-        return new GridFsTemplate(mongoDbFactory(), mongoTemplate().getConverter());
+    public ImmutableMap<String, MongoTemplate> mongoTemplateRegister() throws Exception {
+        return mongoTemplateRegister;
+    }
+
+    private MongoProperties getMongoProperties(String dbPrefix) {
+        MongoProperties mongoProperties = new MongoProperties();
+        mongoProperties.setHost(propertyResolver.getProperty(dbPrefix + "host"));
+        mongoProperties.setPort(propertyResolver.getProperty(dbPrefix + "port", Integer.class));
+        mongoProperties.setDatabase(propertyResolver.getProperty(dbPrefix + "database"));
+        mongoProperties.setAuthenticationDatabase(propertyResolver.getProperty(dbPrefix + "authenticationDatabase"));
+        mongoProperties.setGridFsDatabase(propertyResolver.getProperty(dbPrefix + "gridFsDatabase"));
+        mongoProperties.setPassword(propertyResolver.getProperty(dbPrefix + "password", char[].class));
+        mongoProperties.setUsername(propertyResolver.getProperty(dbPrefix + "username"));
+        mongoProperties.setUri(propertyResolver.getProperty(dbPrefix + "uri"));
+        return mongoProperties;
+    }
+
+    @PostConstruct
+    private void init() throws Exception {
+        Map<String, GridFsTemplate> gridFsTemplateRegister = Maps.newHashMap();
+        Map<String, MongoTemplate> mongoTemplateRegister = Maps.newHashMap();
+        for (String siteName : siteNames) {
+            String dbPrefix = "sites." + siteName + ".db.";
+            MongoProperties mongoProperties = getMongoProperties(dbPrefix);
+            Mongo mongo = mongoProperties.createMongoClient(null);
+            AbstractMongoConfiguration configuration = new AbstractMongoConfiguration() {
+                @Override
+                protected String getDatabaseName() {
+                    return propertyResolver.getProperty(dbPrefix + "database");
+                }
+
+                @Override
+                public Mongo mongo() throws Exception {
+                    return mongo;
+                }
+            };
+            mongoTemplateRegister.put(siteName, configuration.mongoTemplate());
+            gridFsTemplateRegister.put(siteName, new GridFsTemplate(configuration.mongoDbFactory(), configuration.mongoTemplate().getConverter()));
+        }
+        DatabaseConfiguration.this.gridFsTemplateRegister = ImmutableMap.copyOf(gridFsTemplateRegister);
+        DatabaseConfiguration.this.mongoTemplateRegister = ImmutableMap.copyOf(mongoTemplateRegister);
+    }
+
+    @PreDestroy
+    public void close() {
+        for (AbstractMongoConfiguration configuration : configurations) {
+            try {
+                Mongo mongo = configuration.mongo();
+                if (mongo != null) {
+                    mongo.close();
+                }
+            } catch (Exception e) {
+                log.error("Error when close db connecion", e);
+            }
+        }
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        propertyResolver = new RelaxedPropertyResolver(environment);
+        Map<String, Object> sites = propertyResolver.getSubProperties("sites");
+        for (Map.Entry<String, Object> entry : sites.entrySet()) {
+            Iterable<String> split = Splitter.on('.').split(entry.getKey());
+            siteNames.add(Iterables.get(split, 1));
+        }
     }
 }
--- src/main/java/net/org/selector/storer/security/UserDetailsService.java
+++ src/main/java/net/org/selector/storer/security/UserDetailsService.java
@@ -1,17 +1,32 @@
 package net.org.selector.storer.security;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableMap;
+import com.mongodb.BasicDBObject;
+import org.joda.time.DateTime;
 import org.springframework.boot.bind.RelaxedPropertyResolver;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.core.env.Environment;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Authenticate a user from the database.
@@ -20,15 +35,33 @@ import java.util.Set;
 public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService, EnvironmentAware {
     private static final String ENV_SPRING_SITE = "site.";
     @Inject
-    private RestTemplate restTemplate;
-    private RelaxedPropertyResolver propertyResolver;
+    private ImmutableMap<String, MongoTemplate> mongoTemplateRegister;
 
+    @Inject
+    private RestTemplate restTemplate;
+
+    private RelaxedPropertyResolver propertyResolver;
+    private ObjectMapper mapper = new ObjectMapper();
 
     @Override
-    public UserDetails loadUserByUsername(final String login) {
-        ResponseEntity<String> test = restTemplate.getForEntity(propertyResolver.getProperty("userDetailsUrl") + login, String.class);
-        ResponseEntity<UserDetailsDTO> forEntity = restTemplate.getForEntity(propertyResolver.getProperty("userDetailsUrl") + login, UserDetailsDTO.class);
-        return forEntity.getBody().toUserDetails();
+    public UserDetails loadUserByUsername(String login) {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        String site = "petsroom";
+        login = "user";
+        String result = mongoTemplateRegister.get(site).getDb().getCollection("T_USER").findOne(new BasicDBObject("login", login)).toString();
+        try {
+            UserDTO userDetailsDTO = mapper.readValue(result, UserDTO.class);
+            List<GrantedAuthority> grantedAuthorities = userDetailsDTO.authorities.stream()
+                .map(authority -> new SimpleGrantedAuthority(authority.name))
+                .collect(Collectors.toList());
+            User user = new User(userDetailsDTO.login, userDetailsDTO.password, grantedAuthorities);
+//            ResponseEntity<String> test = restTemplate.getForEntity(propertyResolver.getProperty("userDetailsUrl") + login, String.class);
+//            ResponseEntity<UserDetailsDTO> forEntity = restTemplate.getForEntity(propertyResolver.getProperty("userDetailsUrl") + login, UserDetailsDTO.class);
+            return user;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Override
@@ -36,29 +69,37 @@ public class UserDetailsService implements org.springframework.security.core.use
         this.propertyResolver = new RelaxedPropertyResolver(environment, ENV_SPRING_SITE);
     }
 
-    private static class UserDetailsDTO {
-        public String password;
-        public String username;
-        public Set<GrantedAuthorityDTO> authorities;
-        public boolean accountNonExpired;
-        public boolean accountNonLocked;
-        public boolean credentialsNonExpired;
-        public boolean enabled;
-
-
-        public UserDetails toUserDetails() {
-            return new User(this.username, this.password, this.enabled, this.accountNonExpired, this.credentialsNonExpired,
-                this.accountNonLocked, this.authorities);
+    public static class UserDTO implements Serializable {
+        public UserDTO() {
         }
 
-        private static class GrantedAuthorityDTO implements GrantedAuthority {
-            public String authority;
+        @JsonProperty("_id")
+        private String id;
+        private String login;
+        private String password;
+        private String first_name;
+        private String last_name;
+        private String email;
+        private String avatar;
+        private boolean activated = false;
+        private String lang_key;
+        private String activation_key;
+        private String third_party_token;
+        private Set<Authority> authorities = new HashSet<>();
+        private String created_by;
+        private DateTime created_date = DateTime.now();
+        private String last_modified_by;
+        private DateTime last_modified_date = DateTime.now();
 
-            @Override
-            public String getAuthority() {
-                return authority;
-            }
+        public static class Authority implements Serializable {
+            public String name;
         }
+    }
+
+
+    @PostConstruct
+    private void init() {
+//        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 
 }
