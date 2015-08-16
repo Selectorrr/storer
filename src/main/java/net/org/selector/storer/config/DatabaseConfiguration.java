4e5e4f4c6d0e7b9e4909b3a185332a62ae79429c
Stepan Litvinov <litvinovsd@gmail.com>
2015-08-16 22:19:41 +0300

    mongodb version update

--- src/main/java/net/org/selector/storer/config/DatabaseConfiguration.java
+++ src/main/java/net/org/selector/storer/config/DatabaseConfiguration.java
@@ -53,7 +53,7 @@ public class DatabaseConfiguration implements EnvironmentAware {
         mongoProperties.setDatabase(propertyResolver.getProperty(dbPrefix + "database"));
         mongoProperties.setAuthenticationDatabase(propertyResolver.getProperty(dbPrefix + "authenticationDatabase"));
         mongoProperties.setGridFsDatabase(propertyResolver.getProperty(dbPrefix + "gridFsDatabase"));
-        mongoProperties.setPassword(propertyResolver.getProperty(dbPrefix + "password", char[].class));
+        mongoProperties.setPassword(propertyResolver.getProperty(dbPrefix + "password", String.class).toCharArray());
         mongoProperties.setUsername(propertyResolver.getProperty(dbPrefix + "username"));
         mongoProperties.setUri(propertyResolver.getProperty(dbPrefix + "uri"));
         return mongoProperties;
